c4b632e5e6b0214c8fe693906a920cb6876fe06f
Rupesh0913 <63779583+Rupesh0913@users.noreply.github.com>
2021-06-26 19:50:45 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+function Header(){
+  return(
+    <header>
+      <h1>BOOTCAMP with ShapeAI</h1>
+      </header>
+  );
+}
+export default Header
---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+function Info(){
+  return(
+    <div className="note">
+    <h1>JavaScript and React.js</h1>
+    <p>A basci web dev React Bootcamp</p>
+    </div>
+  );
+}
+export default Info
